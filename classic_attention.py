bc925f9bd0ad01574330b3512af6b5ce621f5d78
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:44:47 -0400

    add sampler to the main function

--- classic_attention.py
+++ classic_attention.py
@@ -1,4 +1,5 @@
 from data import attention_data
+from rbms import sampler
 
 TRAIN_SIZE = 100
 TEST_SIZE = 30
@@ -6,6 +7,7 @@ WIDTH = HEIGHT = 100
 
 def main():
     dataset = attention_data.AttentionData(TRAIN_SIZE, TEST_SIZE, WIDTH, HEIGHT)
+    rbm_sampler = sampler.SamplerRBM()
 
 if __name__ == "__main__":
     main()
