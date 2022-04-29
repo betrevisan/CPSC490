f0c796242001558d18d56744c26edd05cd1a77e9
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:31:05 -0400

    add sample_hidden function

--- rbms/sampler.py
+++ rbms/sampler.py
@@ -1,3 +1,11 @@
+import torch
 class SamplerRBM:
     def __init__(self):
         return
+    
+    def sample_hidden(self, model, visible_input):
+        weighted_input = torch.mm(visible_input, model.weights.t())
+        activation = weighted_input + model.hidden_bias.expand_as(weighted_input)
+        prob_h_given_v = torch.sigmoid(activation)
+        #return prob_h_given_v, torch.bernoulli(prob_h_given_v)
+        return prob_h_given_v, self.layer_given_prob(prob_h_given_v)
