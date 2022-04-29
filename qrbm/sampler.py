fa04b1fd8746ac77c139ad1740507510ee1bb75e
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 15:02:33 -0400

    implement sample hidden for quantum

--- qrbm/sampler.py
+++ qrbm/sampler.py
@@ -40,3 +40,38 @@ class Sampler:
         visible_layer = [v for (k, v) in solution_list]
         
         return visible_layer
+    
+    def sample_hidden(self, model, visible_layer):
+        hamiltonian = 0
+        hamiltonian_vars = []
+        
+        # Initialize all variables in the hidden layer
+        for hidden_unit in range(len(model.hidden_bias)):
+            hamiltonian_vars.append(Binary(str(hidden_unit)))
+        
+        for visible_unit in range(len(model.visible_bias)):
+            # Add a connection only to the units that were activated
+            if visible_layer[visible_unit]:
+                for hidden_unit in range(len(model.hidden_bias)):
+                    hamiltonian += -1 * self.weights[visible_unit][hidden_unit] * hamiltonian_vars[hidden_unit]
+              
+        for hidden_unit in range(len(model.hidden_bias)):
+            hamiltonian += -1 * model.hidden_bias[hidden_unit] * hamiltonian_vars[hidden_unit]
+        
+        compiled_hamiltonian = hamiltonian.compile()
+        
+        bqm = compiled_hamiltonian.to_bqm()
+        
+        quantum_sampler = EmbeddingComposite(DWaveSampler())
+        
+        # CONFIRM CHAIN STRENGTH
+        # CONFIRM NUM_READS
+        output = quantum_sampler.sample(bqm, 2, 1)
+        solution = output.first.sample
+        
+        # REVISIT THIS
+        solution_list = [(k, v) for k, v in solution.items()]
+        solution_list.sort(key=lambda tup: int(tup[0]))  # sorts in place
+        hidden_layer = [v for (k, v) in solution_list]
+        
+        return hidden_layer
