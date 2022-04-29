3a64a3cef316400611557420ee50dc8b0f4ac846
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:27:24 -0400

    implement train function

--- rbms/attention_rbm.py
+++ rbms/attention_rbm.py
@@ -7,3 +7,9 @@ class AttentionRBM:
         self.hidden_bias = torch.randn(1, hidden_dim)
         self.visible_bias = torch.randn(1, visible_dim)
         self.learning_rate = 0.1
+    
+    def train(self, initial_visible, curr_visible, initial_prob_h, curr_prob_h):
+        # CONFIRM THE WEIGHT UPDATE IS CORRECT
+        self.weights += torch.mm(initial_prob_h.t(), initial_visible) - torch.mm(curr_prob_h.t(), curr_visible)
+        self.visible_bias += torch.sum((initial_visible - curr_visible), 0)
+        self.hidden_bias += torch.sum((initial_prob_h - curr_prob_h), 0)
