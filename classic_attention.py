7cd2b1255eb9324cf36a292aa14ea51f77beba00
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:48:53 -0400

    initialize rbm and train it

--- classic_attention.py
+++ classic_attention.py
@@ -1,13 +1,21 @@
 from data import attention_data
-from rbms import sampler
+from rbms import sampler, attention_rbm
 
 TRAIN_SIZE = 100
 TEST_SIZE = 30
 WIDTH = HEIGHT = 100
+EPOCHS = 20
 
 def main():
     dataset = attention_data.AttentionData(TRAIN_SIZE, TEST_SIZE, WIDTH, HEIGHT)
+
     rbm_sampler = sampler.SamplerRBM()
+
+    visible_dim = len(dataset.train_data_bin[0])
+    hidden_dim = 15
+    rbm = attention_rbm.AttentionRBM(rbm_sampler, visible_dim, hidden_dim)
+
+    rbm.train(dataset.test_data_bin, EPOCHS)
 
 if __name__ == "__main__":
     main()
