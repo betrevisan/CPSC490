35943afeca44913af47f89992f0ad963bca456fe
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:31:53 -0400

    implement sample visible function

--- rbms/sampler.py
+++ rbms/sampler.py
@@ -7,5 +7,10 @@ class SamplerRBM:
         weighted_input = torch.mm(visible_input, model.weights.t())
         activation = weighted_input + model.hidden_bias.expand_as(weighted_input)
         prob_h_given_v = torch.sigmoid(activation)
-        #return prob_h_given_v, torch.bernoulli(prob_h_given_v)
         return prob_h_given_v, self.layer_given_prob(prob_h_given_v)
+    
+    def sample_visible(self, model, hidden_input):
+        weighted_input = torch.mm(hidden_input, model.weights)
+        activation = weighted_input + model.visible_bias.expand_as(weighted_input)
+        prob_v_given_h = torch.sigmoid(activation)
+        return prob_v_given_h, self.layer_given_prob(prob_v_given_h)
