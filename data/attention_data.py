af3cd5ff9628b4e7946df91a9a59750223e0e1c8
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-29 01:07:51 -0400

    implement generate train data function

--- data/attention_data.py
+++ data/attention_data.py
@@ -9,11 +9,32 @@ class AttentionData:
         self.test_size = test_size
         self.width = width
         self.height = height
-        # self.train_data = self.generate_train_data()
+        self.train_data = self.generate_train_data()
         # self.test_data = self.generate_test_data()
         # self.train_data_bin = self.prepare_data(self.train_data)
         # self.test_data_bin = self.prepare_data(self.test_data)
         # self.train_data_bin_answers = self.prepare_train_answers()
         # self.test_data_bin_answers = self.prepare_test_answers()
+
+    # Generate training data for the network
+    def generate_train_data(self):
+        # Each datapoint follows the format [prey_loc, agent_loc, predator_loc, prey_attn, agent_attn, predator_attn]
+        data = []
+        for _ in range(self.train_size):
+            datapoint = []
+            # Generate random positions for the agent, prey, and predator
+            # Prey's position
+            datapoint.append(self.generate_random_loc(0, self.width/3))
+            # Agent's position
+            datapoint.append(self.generate_random_loc(self.width/3, 2*self.width/3))
+            # Predator's position
+            datapoint.append(self.generate_random_loc(2*self.width/3, self.width))
+
+            # Get the best attention allocations for the given positions
+            datapoint.append(self.best_attention(datapoint))
+            
+            data.append(datapoint)
+        
+        return np.array(data)
 
     
