86c5ac9f4846540a752d224a21dd283019dd6701
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-30 12:27:32 -0400

    comments

---
+++ quantum.py
@@ -0,0 +1,4 @@
+""" Complete Predator-Prey Task (Quantum Approach)
+This implements the complete Predator-Prey task within quantum computing using a
+quantum deep restricted boltzmann machine sampled with quantum annealing.
+"""
