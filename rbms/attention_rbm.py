f934ddb13b2a97cc8f0a1b592dfe7ef561904da0
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-04-30 14:37:00 -0400

    comments

--- rbms/attention_rbm.py
+++ rbms/attention_rbm.py
@@ -1,8 +1,50 @@
 import numpy as np
 
 class AttentionRBM:
+    """
+    The class AttentionRBM class describes the restricted boltzmann machine used
+    by the attention allocation model for the classic approach to the problem.
+    ...
+    Attributes
+    ----------
+    sampler : SamplerRBM
+        RBM sampler
+    weigths : np.Array of size (visible_dim, hidden_dim)
+        Weights of the connections in the RBM
+    visible_bias : np.Array of size (visible_dim)
+        Biases in the visible layer of the RBM
+    hidden_bias : np.Array of size (hidden_dim)
+        Biases in the hidden layer of the RBM
+
+    Methods
+    -------
+    train(train_data, epochs, learning_rate)
+        Trains the RBM model given the number of epochs and the learning rate.
+    update_weights(init_visible, init_hidden, curr_visible, curr_hidden, learning_rate)
+        Update the RBM's weights given the initial visible and hidden layers, the
+        current visible and hidden layers, and the learning rate.
+    update_visible_bias(init_visible, curr_visible, learning_rate)
+        Update the biases of the visible layer given the initial visible layer, current
+        visible layer, and learning rate.
+    update_hidden_bias(init_hidden, curr_hidden, learning_rate)
+        Update the biases of the hidden layer given the initial hidden layer, current
+        hidden layer, and learning rate.
+    test(test_data, test_answers)
+        Test the RBM model.
+    """
 
     def __init__(self, sampler, visible_dim, hidden_dim):
+        """
+        Parameters
+        ----------
+        sampler : SamplerRBM
+            RBM sampler
+        visible_dim : int
+            Number of nodes in the visible dimension of the RBM
+        hidden_dim : int
+            Number of nodes in the hidden dimension of the RBM
+        """
+
         self.sampler = sampler
         self.weights = np.random.rand(visible_dim, hidden_dim)
         self.visible_bias = np.random.rand(visible_dim)
@@ -10,14 +52,30 @@ class AttentionRBM:
         return
     
     def train(self, train_data, epochs, learning_rate):
+        """Trains the RBM model given the number of epochs and the learning rate.
+        Parameters
+        ----------
+        train_data : np.Array
+            The training data
+        epochs : int
+            Number of epochs to train for
+        learning_rate : float
+            Learning rate for training
+        Returns
+        -------
+        None
+        """
+        # Repeat for each epoch
         for epoch in range(epochs):
             train_loss = 0
             n = 0.0
+
+            # Go over each data point in the training data
             for i in range(len(train_data)):
                 init_visible = train_data[i]
-                init_prob_h, init_hidden = self.sampler.sample_hidden(self, init_visible)
-                curr_prob_v, curr_visible = self.sampler.sample_visible(self, init_hidden)
-                curr_prob_h, curr_hidden = self.sampler.sample_hidden(self, curr_visible)
+                init_hidden = self.sampler.sample_hidden(self, init_visible)
+                curr_visible = self.sampler.sample_visible(self, init_hidden)
+                curr_hidden = self.sampler.sample_hidden(self, curr_visible)
 
                 self.update_weights(init_visible, init_hidden, curr_visible, curr_hidden, learning_rate)
             
@@ -31,20 +89,77 @@ class AttentionRBM:
         return
     
     def update_weights(self, init_visible, init_hidden, curr_visible, curr_hidden, learning_rate):
+        """Update the RBM's weights given the initial visible and hidden layers, the
+        current visible and hidden layers, and the learning rate.
+        Parameters
+        ----------
+        init_visible : np.Array
+            The initial visible layer
+        init_hidden : np.Array
+            The initial hidden layer
+        curr_visible : np.Array
+            The current visible layer
+        curr_hidden : np.Array
+            The current hidden layer
+        learning_rate : float
+            Learning rate for weight updates
+        Returns
+        -------
+        None
+        """
         positive_gradient = np.outer(init_visible, init_hidden)
         negative_gradient = np.outer(curr_visible, curr_hidden)
         self.weights += learning_rate * (positive_gradient - negative_gradient)
         return
 
     def update_visible_bias(self, init_visible, curr_visible, learning_rate):
+        """Update the biases of the visible layer given the initial visible layer, current
+        visible layer, and learning rate.
+        Parameters
+        ----------
+        init_visible : np.Array
+            The initial visible layer
+        curr_visible : np.Array
+            The current visible layer
+        learning_rate : float
+            Learning rate for bias updates
+        Returns
+        -------
+        None
+        """
         self.visible_bias += learning_rate * (init_visible - curr_visible)
         return
 
     def update_hidden_bias(self, init_hidden, curr_hidden, learning_rate):
+        """Update the biases of the hidden layer given the initial hidden layer, current
+        hidden layer, and learning rate.
+        Parameters
+        ----------
+        init_hidden : np.Array
+            The initial hidden layer
+        curr_hidden : np.Array
+            The current hidden layer
+        learning_rate : float
+            Learning rate for bias updates
+        Returns
+        -------
+        None
+        """
         self.hidden_bias += learning_rate * (init_hidden - curr_hidden)
         return
 
     def test(self, test_data, test_answers):
+        """Test the RBM model.
+        Parameters
+        ----------
+        test_data : np.Array
+            The test data
+        test_answers : np.Array
+            The answers to the test data
+        Returns
+        -------
+        None
+        """
         # Testing the RBM Model
         test_loss = 0
         n = 0.
