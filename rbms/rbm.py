538900ff36b7bd5753c7d25fc7b2ad37c64790a1
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-05-03 13:18:00 -0400

    implement test

--- rbms/rbm.py
+++ rbms/rbm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class RBM:
     """
@@ -65,4 +66,140 @@ class RBM:
         self.visible_bias = np.random.rand(visible_dim)
         self.hidden_bias = np.random.rand(hidden_dim)
         return
+
+    def train(self, train_data, epochs, learning_rate):
+        """Trains the RBM model given the number of epochs and the learning rate.
+        Parameters
+        ----------
+        train_data : np.Array
+            The training data
+        epochs : int
+            Number of epochs to train for
+        learning_rate : float
+            Learning rate for training
+        Returns
+        -------
+        None
+        """
+        # Repeat for each epoch
+        for epoch in range(epochs):
+            datapoint_index = random.randrange(0, len(train_data))
+            
+            init_visible = train_data[datapoint_index]
+            init_hidden = self.sampler.sample_hidden(self, init_visible)
+
+            curr_visible = self.sampler.sample_visible(self, init_hidden)
+            curr_hidden = self.sampler.sample_hidden(self, curr_visible)
+
+            # Update weights
+            self.update_weights(init_visible, init_hidden, curr_visible, curr_hidden, learning_rate)
+
+            # Update visible bias
+            self.update_visible_bias(init_visible, curr_visible, learning_rate)
+
+            # Update hidden bias
+            self.update_hidden_bias(init_hidden, curr_hidden, learning_rate)
+
+            error = self.error(init_visible[42:], curr_visible[42:])            
+            print("epoch: " + str(epoch) + " loss: " + str(error))
+        return
+
+    def update_weights(self, init_visible, init_hidden, curr_visible, curr_hidden, learning_rate):
+        """Update the RBM's weights given the initial visible and hidden layers, the
+        current visible and hidden layers, and the learning rate.
+        Parameters
+        ----------
+        init_visible : np.Array
+            The initial visible layer
+        init_hidden : np.Array
+            The initial hidden layer
+        curr_visible : np.Array
+            The current visible layer
+        curr_hidden : np.Array
+            The current hidden layer
+        learning_rate : float
+            Learning rate for weight updates
+        Returns
+        -------
+        None
+        """
+        positive_gradient = np.outer(init_visible, init_hidden)
+        negative_gradient = np.outer(curr_visible, curr_hidden)
+        self.weights += learning_rate * (positive_gradient - negative_gradient)
+        return
+    
+    def update_visible_bias(self, init_visible, curr_visible, learning_rate):
+        """Update the biases of the visible layer given the initial visible layer, current
+        visible layer, and learning rate.
+        Parameters
+        ----------
+        init_visible : np.Array
+            The initial visible layer
+        curr_visible : np.Array
+            The current visible layer
+        learning_rate : float
+            Learning rate for bias updates
+        Returns
+        -------
+        None
+        """
+        self.visible_bias += learning_rate * (init_visible - curr_visible)
+        return
+
+    def update_hidden_bias(self, init_hidden, curr_hidden, learning_rate):
+        """Update the biases of the hidden layer given the initial hidden layer, current
+        hidden layer, and learning rate.
+        Parameters
+        ----------
+        init_hidden : np.Array
+            The initial hidden layer
+        curr_hidden : np.Array
+            The current hidden layer
+        learning_rate : float
+            Learning rate for bias updates
+        Returns
+        -------
+        None
+        """
+        self.hidden_bias += learning_rate * (init_hidden - curr_hidden)
+        return
+    
+    def test(self, test_data, test_answers):
+        """Test the RBM model.
+        Parameters
+        ----------
+        test_data : np.Array
+            The test data
+        test_answers : np.Array
+            The answers to the test data
+        Returns
+        -------
+        None
+        """
+        # Testing the RBM Model
+        error = 0
+        for i in range(len(test_data)):
+            visible = test_data[i]
+            visible_answer = test_answers[i]
+            hidden = self.sampler.sample_hidden(self, visible)
+            visible = self.sampler.sample_visible(self, hidden)
+            error += self.error(visible_answer, visible[42:])
+        print("test loss: " + str(error/len(test_data)))
+        return
+    
+    def error(self, answer, prediction):
+        """Get the error given the actual answer and the model's prediction.
+        Parameters
+        ----------
+        answer : np.Array
+            The actual correct answer
+        prediction : np.Array
+            The model's prediction
+        Returns
+        -------
+        float
+            The error between the actual answer and the prediction
+        """
+        error = np.mean(np.abs(answer - prediction))
+        return error
     
