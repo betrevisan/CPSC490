f31473cf2c924227018c6e1207b72b4c9258fde1
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2022-05-05 00:58:42 -0400

    parallel metrics

--- parallel/classic.py
+++ parallel/classic.py
@@ -3,12 +3,14 @@ This implements the complete Predator-Prey task within classical computing using
 restricted boltzmann machine.
 """
 
+import time
 from data import data
 from rbms import rbm
 from samplers import classic_sampler
 from characters import agent as agent_mod
 from characters import predator as predator_mod
 from characters import prey as prey_mod
+from metrics import metrics as metrics_mod
 
 TRAIN_SIZE = 100
 TEST_SIZE = 30
@@ -16,9 +18,12 @@ MAX_SPEED = 5
 WIDTH = HEIGHT = 100
 EPOCHS = 20
 LEARNING_RATE = 0.1
-ITERATIONS = 1 # Number of iterations in the game
+ITERATIONS = 35 # Number of iterations in the game
 
 def main():
+    # Initialize metrics instance
+    metrics = metrics_mod.Metrics("Parallel Classical Implementation")
+
     # Generate a data for training and testing given the width and the height of the 
     # coordinate plane and the maximum speed
     dataset = data.Data(TRAIN_SIZE, TEST_SIZE, MAX_SPEED, WIDTH, HEIGHT)
@@ -33,8 +38,12 @@ def main():
     # Initialize the RBM given the sampler and the dimensions
     rbm_model = rbm.RBM(rbm_sampler, visible_dim, hidden_dim)
 
+    # Start the training timer (must be adjusted from seconds to microseconds)
+    training_start_time = time.time() * 1000000
     # Train the RBM for a given number of epochs with a given learning rate
     rbm_model.train(dataset.test_data_bin, EPOCHS, 0.1)
+    # Record time elapsed during training
+    metrics.training_time += (time.time() * 1000000 - training_start_time)
 
     # Test the RBM
     rbm_model.test(dataset.test_data_bin, dataset.test_data_bin_answers)
@@ -49,10 +58,47 @@ def main():
     # Run model for n iterations
     for _ in range(ITERATIONS):
 
+        # Start the movement timer
+        decision_start_time = time.time() * 1000000
         # Moves the agent according to the boltzmann
         rbm_model.move_locs(agent, prey.loc, predator.loc, MAX_SPEED)
+        # Record time elapsed during decision
+        metrics.decision_time += (time.time() * 1000000 - decision_start_time)
 
-    print(agent)
+    # Add general metrics
+    metrics.w = WIDTH
+    metrics.h = HEIGHT
+    metrics.iterations = ITERATIONS
+    metrics.training_size = TRAIN_SIZE
+    metrics.test_size = TEST_SIZE
+    metrics.max_speed = MAX_SPEED
+    metrics.learning_rate = LEARNING_RATE
+    metrics.epochs = EPOCHS
+
+    # Add agent to metrics
+    metrics.agent_alive = agent.alive
+    metrics.agent_feasted = agent.feasted
+    metrics.agent_loc_trace = agent.loc_trace
+    metrics.agent_perceived_loc_trace = agent.perceived_agent_trace
+    metrics.prey_perceived_loc_trace = agent.perceived_prey_trace
+    metrics.predator_perceived_loc_trace = agent.perceived_predator_trace
+    metrics.dist_agent2prey_trace = [dist[0] for dist in agent.dist_trace]
+    metrics.dist_agent2predator_trace = [dist[1] for dist in agent.dist_trace]
+
+    # Add prey to metrics
+    metrics.prey_alive = prey.alive
+    metrics.prey_loc_trace = prey.loc_trace
+
+    # Add predator to metrics
+    metrics.predator_feasted = predator.feasted
+    metrics.predator_loc_trace = predator.loc_trace
+
+    # Add attention trace to metrics
+    metrics.attention_trace = agent.attn_trace
+
+    # Display metrics
+    print(metrics)
+
     return
 
 
---
+++ parallel/metrics/metrics.py
@@ -0,0 +1,151 @@
+class Metrics:
+
+    """
+    The Metrics class carries several metrics corresponding to an implementation
+    of the predator-prey task.
+    ...
+    Attributes
+    ----------
+    name : str
+        Name of the implementation
+    Methods
+    -------
+    """
+
+    def __init__(self, name):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the implementation
+        """
+        self.name = name
+        self.w = 0
+        self.h = 0
+        self.iterations = 0
+        self.max_speed = -1
+        self.num_reads = -1
+        self.training_time = 0
+        self.epochs = 0
+        self.learning_rate = 0
+        self.training_size = 0
+        self.test_size = 0
+        self.decision_time = 0
+        self.total_sampling_time_attn = 0
+        self.total_anneal_time_attn = 0
+        self.total_readout_time_attn = 0
+        self.total_sampling_time_move = 0
+        self.total_anneal_time_move = 0
+        self.total_readout_time_move = 0
+        self.attention_trace = []
+        self.agent_alive = True
+        self.agent_feasted = False
+        self.agent_loc_trace = []
+        self.agent_perceived_loc_trace = []
+        self.dist_agent2prey_trace = []
+        self.dist_agent2predator_trace = []
+        self.prey_alive = True
+        self.prey_loc_trace = []
+        self.prey_perceived_loc_trace = []
+        self.predator_feasted = False
+        self.predator_loc_trace = []
+        self.predator_perceived_loc_trace = []
+        return
+    
+    def __repr__(self):
+        """Displays information about the implementation
+        """
+        display = ['\n===============================']
+        display.append(" ".join(self.name.upper()) + "\n")
+        display.append('General Metrics')
+        display.append("\tWidth x Height:                                    " + str(self.w) + " x " + str(self.h))
+        display.append("\tIterations:                                        " + str(self.iterations))
+        display.append("\tMax speed:                                        " + str(self.max_speed))
+        if self.num_reads != -1:
+            display.append("\tAnnealer reads per iteration:                      " + str(self.num_reads))
+
+        display.append('\nTraining Metrics')
+        display.append('\tEpochs:                                             ' + str(self.epochs))
+        display.append('\tLearning Rate:                                             ' + str(self.learning_rate))
+        display.append('\tTraining Size:                                             ' + str(self.training_size))
+        display.append('\tTest Size:                                             ' + str(self.test_size))
+
+        display.append('\nTime Metrics (in microseconds)')
+        if self.training_time > 0:
+            display.append("\t\tTraining time:                            " + "{:.2f}".format(self.training_time))
+        if self.decision_time > 0:
+            display.append("\t\tAverage Decision time:                            " + "{:.2f}".format(self.decision_time/self.iterations))
+        if self.total_sampling_time_attn > 0:
+            display.append("\t\tAttention sampling time:                            " + "{:.2f}".format(self.total_sampling_time_attn/self.iterations))
+            display.append("\t\tAttention anneal time:                             " + "{:.2f}".format(self.total_anneal_time_attn/self.iterations))
+            display.append("\t\tAttention readout time:                            " + "{:.2f}".format(self.total_readout_time_attn/self.iterations))
+            display.append("\t\tMovement sampling time:                             " + "{:.2f}".format(self.total_sampling_time_move/self.iterations))
+            display.append("\t\tMovement anneal time:                            " + "{:.2f}".format(self.total_anneal_time_move/self.iterations))
+            display.append("\t\tMovement readout time:                             " + "{:.2f}".format(self.total_readout_time_move/self.iterations))
+
+
+        display.append('\nAttention Allocation Metrics')
+        trace_str = "\tTrace:                                             "
+        for attn in self.attention_trace:
+            trace_str += ", " + str(attn)
+        display.append(trace_str.replace(", ","",1))
+
+        display.append('\nAgent Metrics')
+        display.append('\tAlive:                                             ' + str(self.agent_alive))
+        display.append('\tFeasted:                                           ' + str(self.agent_feasted))
+        display.append('\tSteps taken:                                       ' + str(len(self.agent_loc_trace) - 1))
+        trace_str = "\tLocation trace:                                    "
+        for loc in self.agent_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+        trace_str = "\tAgent perceived location trace:                    "
+        for loc in self.agent_perceived_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+        trace_str = "\tDistance to prey trace:                            "
+        for dist in self.dist_agent2prey_trace:
+            trace_str += ", " + "{:.2f}".format(dist)
+        display.append(trace_str.replace(", ","",1))
+        trace_str = "\tDistance to predator trace:                        "
+        for dist in self.dist_agent2predator_trace:
+            trace_str += ", " + "{:.2f}".format(dist)
+        display.append(trace_str.replace(", ","",1))
+
+        display.append('\nPrey Metrics')
+        display.append('\tAlive:                                             ' + str(self.prey_alive))
+        display.append('\tSteps taken:                                       ' + str(len(self.prey_loc_trace) - 1))
+        trace_str = "\tLocation trace:                                    "
+        for loc in self.prey_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+        trace_str = "\tAgent's perceived prey location trace:             "
+        for loc in self.prey_perceived_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+
+        display.append('\nPredator Metrics')
+        display.append('\tFeasted:                                           ' + str(self.predator_feasted))
+        display.append('\tSteps taken:                                       ' + str(len(self.predator_loc_trace) - 1))
+        trace_str = "\tLocation trace:                                    "
+        for loc in self.predator_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+        trace_str = "\tAgent's perceived predator location trace:         "
+        for loc in self.predator_perceived_loc_trace:
+            loc[0] = "{:.2f}".format(loc[0])
+            loc[1] = "{:.2f}".format(loc[1])
+            trace_str += ", " + str(loc)
+        display.append(trace_str.replace(", ","",1))
+
+        display.append('===============================\n')
+        return "\n".join(display)
